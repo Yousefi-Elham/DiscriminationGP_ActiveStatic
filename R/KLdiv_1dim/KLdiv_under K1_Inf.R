6f9a96fa05c3e8e0c584964adc71a4ebf50e48f1
Elham <elhamyousefi1st@gmail.com>
2023-10-06 13:17:11 +0200

    KL divergence 1dim example added_2

--- R/KLdiv_1dim/KLdiv_under K1_Inf.R
+++ R/KLdiv_1dim/KLdiv_under K1_Inf.R
@@ -1,3 +1,4 @@
+
 library(plgp)
 library(mvtnorm)
 library(MASS)
@@ -7,28 +8,27 @@ rm(list=ls())
 par(mfrow=c(1,1))
 
 
-#---------------------------------generate the obs from under each kernel--------------------------
+#---------------------------------design size and par setting-------------------
 sigma2.ini=1
-rho.ini1=0.6
-rho.ini2=0.2
 grid <- seq(0,1,by=0.01)
 nexc <- N <- length(grid)
 
-#---------------------------------------------------------------------------------------------
-#actin <- 51
+#-------------------------------------------------------------------------------
+actin <- 51
 #actin=c(11,51,91)
-set.seed(123)
-actin=sample(1:N,3) 
-
-est.rhos <- c(0.6,0.2)
+#set.seed(123456789)
+#actin=c(20,34,89)
 indd <- 0
-#---------------------------------plot (and construct) starting design-----------------
 
+#est.rhos <- c(0.5245718, 0.1191174) #estimated rhos under the first kernel
+#est.rhos <- c(0.7957271, 0.1963281) #estimated rhos under the second kernel
+est.rhos <- c(1,1)
 
+#---------------------------------plot (and construct) starting design----------
 
 plot(seq(1:nexc),seq(0,1,length=nexc), type = "n",xlab="iter",ylab="Incremental design",
      main = bquote( paste("size:", .(nexc),", ","start:", .(paste(grid[actin], collapse=", "))
-                          ,", ", rho, ":", .(paste(round(est.rhos,digits = 2), collapse=", ")),", ", sigma^2, ":",.(sigma2.ini)) ),cex.main=2, cex.lab=1.5, cex.axis=1.5,pch=2,col="blue");
+                          ,", ", rho, ":", .(paste(round(est.rhos,digits = 2), collapse=", ")),", ", sigma^2, ":",.(sigma2.ini)) ),pch=2,col="blue");
 
 
 CRIT.vec <- numeric(nexc)
@@ -48,26 +48,25 @@ KL.crit.fun <- function(rho,sigma2,des){
   kernel1 <- sigma2*exp(-DisM/rho[1])
   kernel2 <- sigma2*(matrix(rep(1,n*n),n,n)+DisM/rho[2]) * exp(-DisM/rho[2])
   
-  
-  kl.crit <- 1/4*( sum(diag(solve(kernel1) %*% kernel2)) + sum(diag(solve(kernel2) %*% kernel1)) ) - n/2
+  #if kernel1 holds
+  kl.crit <- 1/2*( sum(diag(solve(kernel2) %*% kernel1)) ) + 1/2*log(det(kernel2)/det(kernel1)) - n/2
   kl.crit
 }
+
 
 
 n1 <- length(actin)
 xstart <- grid[actin]
 xcan <- grid[-actin]
 #xcan <- sample(xcan)
-CRIT.vec[1:n1] <- KL.crit.fun(est.rhos,sigma2.ini,xstart)
-
-
+CRIT.vec[1:n1] <- as.numeric(KL.crit.fun(est.rhos,sigma2.ini,xstart))
 
 
 for(ii in 1:n1){
   points(ii, xstart[ii],col="darkblue", pch=16,cex=1)
 }
 
-#----------------------cunstructing the rest (of design) up to the required size-------------------
+#-----------constructing the rest (of design) up to the required size-----------
 
 nleft <- nexc-n1
 for( k in 1: nleft){
@@ -79,7 +78,7 @@ for( k in 1: nleft){
   ad.crit <- numeric(Ncan)
   for(m in 1: Ncan){
     test.des <- c(xstart,xcan[m])
-    ad.crit[m] <- KL.crit.fun(est.rhos,sigma2.ini,test.des)
+    ad.crit[m] <- as.numeric(KL.crit.fun(est.rhos,sigma2.ini,test.des))
   }
   ad.ind <- which.max(ad.crit)
   ad.point <- xcan[ad.ind]
@@ -87,7 +86,7 @@ for( k in 1: nleft){
   xstart <- c(xstart,ad.point)
   xcan <- xcan[-ad.ind]
   xcan <- sample(xcan)
-  CRIT.vec[k+n1] <- KL.crit.fun(est.rhos,sigma2.ini,xstart)
+  CRIT.vec[k+n1] <- as.numeric(KL.crit.fun(est.rhos,sigma2.ini,xstart))
   
   if(CRIT.vec[k+n1]>CRIT.vec[k+n1-1]){
     points(k+n1,xhat , type = "p",col="darkblue", pch=16,cex=1 )   
@@ -97,5 +96,5 @@ for( k in 1: nleft){
   }
   
 }
-
+CRIT.vec
 lines(seq(1:N),xstart,col="black", pch=16,cex=1)
